b4d9a00d6cd3dd8167dfadaca64611ff2548aa55
Adam Leuer <aleuer2@illinois.edu>
2021-01-20 17:55:56 -0800

    Reorganized code

--- Cars/Cars/View/Utility/CarItemSelector.cs
+++ Cars/Cars/View/Utility/CarItemSelector.cs
@@ -3,7 +3,7 @@ using Cars.Model;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 
-namespace Cars.Utility
+namespace Cars.View.Utility
 {
 	public class CarItemSelector : DataTemplateSelector
 	{
